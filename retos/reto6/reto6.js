eaae5a01dd16909bd1ffc3715915965db16ce41a
StevenFuertes <sfuertes1994@gmail.com>
2022-09-27 19:39:16 -0500

    "avance-reto6"

---
+++ retos/reto6/reto6.js
@@ -0,0 +1,30 @@
+let number_card= "123";
+let name_card= "nombre";
+let bool =true;
+let simbol = Symbol("abc");
+
+console.log("123",number_card);
+console.error("error!!");
+console.warn("warnings!!");
+
+console.dir()
+
+let numero1 = 1;
+let numero2 = 2;
+
+let total = numero1 + numero2;
+
+console.log("total",total)
+
+let numero3 ="4";
+let numero4 = "2";
+
+let total1 = parseInt(numero3) + parseInt(numero4);
+
+console.log("total1",total1)
+
+function suma(a,b) {
+    return a + b
+}
+
+console.log(suma(2,3));
