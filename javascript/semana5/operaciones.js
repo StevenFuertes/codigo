6e02db0956405892cbfcc6dfb148c086150dcaee
StevenFuertes <sfuertes1994@gmail.com>
2022-09-28 23:06:00 -0500

    "inicio_js"

---
+++ javascript/semana5/operaciones.js
@@ -0,0 +1,17 @@
+let n1 =10;
+let n2 =20;
+
+let verdadero = true; //boolean
+let falso = false;
+
+let nombre = "Steven";
+
+let direccion = null; //
+
+let suma = n1+n2;
+
+let resta = n1-n2;
+
+let producto = n1*n2;
+
+let division = n1/n2;
