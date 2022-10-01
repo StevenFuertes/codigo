ee9ff8b5397c6c1fe78877c643fa80ee29898e02
StevenFuertes <sfuertes1994@gmail.com>
2022-09-30 23:05:30 -0500

    "avance js"

---
+++ javascript/semana5/string.js
@@ -0,0 +1,6 @@
+function stringSplit() {
+    const fullname = prompt("Nombre y apellido: ");
+    // const fullname_fixed = fullname.replace();
+    const array_fullname = fullname.split(" ");
+    console.log(array_fullname);
+}
